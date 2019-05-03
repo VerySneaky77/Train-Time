bd32734a3886a41f7d4ade7b545c91c96d2b2073
Carlos Quinto <carlquin77@gmail.com>
2019-05-03 01:06:08 -0700

    Firebase data storage and reference implemented

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,35 +1,68 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAaqkzl5CZiUmleyeSb7ngZHeaWn5cEy5Q",
+    authDomain: "train-time-schedule-c3d03.firebaseapp.com",
+    databaseURL: "https://train-time-schedule-c3d03.firebaseio.com",
+    projectId: "train-time-schedule-c3d03",
+    storageBucket: "train-time-schedule-c3d03.appspot.com",
+    messagingSenderId: "23222976120"
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database()
+
 $(document).ready(function () {
-    var $nameInput = $("#train-name-input");
-    var $localeInput = $("#train-location-input");
-    var $timeInput = $("#train-time-input");
 
     $("#train-line-form").on("submit", function (e) {
+        var $nameInput = $("#train-name-input");
+        var $localeInput = $("#train-location-input");
+        var $timeInput = $("#train-time-input");
+
         e.preventDefault();
 
-        console.log(dataAuthenticate());
-
-        if (dataAuthenticate()) {
-
+        // Form filled
+        if ($nameInput.val().length > 0 && $localeInput.val().length > 0 && $timeInput.val().length > 2) {
+            // Push to database
+            database.ref().push({
+                name: $nameInput.val().trim(),
+                location: $localeInput.val().trim(),
+                time: $timeInput.val().trim()
+            });
         }
+        else { $("#empty-form-warning").text("You must fill all form fields."); }
     })
 
+    database.ref().on("child_added", function (snapshot) {
+        // Track object properties
+        var trainName = snapshot.val().name;
+        var trainLocation = snapshot.val().location;
+        var trainTime = snapshot.val().time;
 
-    function dataAuthenticate() {
-        if ($nameInput.length > 0 && $localeInput.length > 0 && $timeInput.length > 4) {
-            if (parseInt($timeInput.val()) !== NaN) {
-                return true;
-            }
-            else {
-                $("#empty-form-warning").text("Time must be in a numerical time format with a colon to separate hours and minutes.")
-                return false;
-            }
-        }
-        else {
-            $("#empty-form-warning").text("You must fill all form fields.");
-            return false;
-        }
+        console.log(trainName);
+        console.log(trainLocation);
+        console.log(trainTime);
+
+        // Table well
+        var newRow = $("<tr>").append(
+            $("<td>").text(trainName),
+            $("<td>").text(trainLocation),
+            $("<td>").text(trainTime),
+            $("<td>").text(calculateArrival(trainTime)),
+        );
+
+        $("#trains-table > tbody").prepend(newRow);
+
+    }, function (errorObject) {
+        console.log("Oopsies: " + errorObject.code);
+    });
+
+    // Calculate arrival time
+    function calculateArrival() {
+        return "12"
     }
 
+    // display database entries
     function updateTimeTracker() {
 
     }
